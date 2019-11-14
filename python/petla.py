7770a1dc6f9eab19c561e1eb8e31d765f77e59b2
mkowalikx1337 <mateusz.kowalik@lo1.sandomierz.pl>
2019-11-14 12:33:50 +0100

    algorytmy iteracyjne

--- python/petla.py
+++ python/petla.py
@@ -2,17 +2,28 @@
 # -*- coding: utf-8 -*-
 
 
-def sumuj_parzyste():
+def sumuj_parzyste(start, stop):
     suma = 0
-    for i in range(0, 101, 2):
+    for i in range(start, stop + 1, 2):
         suma = suma + i
     print(suma)
+    return 0
+    
+    
+    
+def drukuj_nieparzyste(start, stop):
+    if (start % 2 == 0):
+        start = start + 1
+    for i in range(start, stop + 1, 2):
     return 0
     
 
 
 def main(args):
-    sumuj_parzyste()
+    start = int(input("Podaj początek zakresu: "))
+    stop = int(input("Podaj koniec zakresu: "))
+    drukuj_nieparzyste(start, stop)
+    #sumuj_parzyste(start, stop)
     #suma = 0
     #for i in range(3):
         #liczba = int(input("Podaj liczbę: "))
