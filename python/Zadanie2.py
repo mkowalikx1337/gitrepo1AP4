d2e3eb6f9a91219a7776aac07af925e0293535ea
mkowalikx1337 <mateusz.kowalik@lo1.sandomierz.pl>
2019-11-20 11:40:45 +0100

    Zadanie2

---
+++ python/Zadanie2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def main(args):
+    liczba = 0
+    n = int(input("Podaj liczbe n: "))
+    m = int(input("Podaj liczbe m: "))
+    for i in range(n, m + 1, 2):
+        liczba = liczba + 1
+    print(liczba)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
