0a058b3414babc4966032b5b361f3aab268e73b6
mkowalikx1337 <mateusz.kowalik@lo1.sandomierz.pl>
2019-11-28 12:34:00 +0100

    Podstawy C++

--- CPP/rownanie.cpp
+++ CPP/rownanie.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    int a;
+    int b;
+    float x;
+    cout << "Podaj liczbę a: ";
+    cin >> a;
+    cout << "Podaj liczbę b: ";
+    cin >> b;
+    if (a == 0){
+        if (b == 0){
+            cout << "Nieskończenie wiele rozwiązań";
+        }
+        else{
+            cout << "Równianie sprzeczne";
+        }
+    }
+    else{
+        x = -b / (float)a;
+        cout << x;
     return 0;
+    }
 }
-
