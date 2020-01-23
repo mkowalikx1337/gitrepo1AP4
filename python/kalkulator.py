3d85b1af8f76180cf610e5f348ad0cece7f5b721
mkowalikx1337 <mateusz.kowalik@lo1.sandomierz.pl>
2020-01-23 12:36:17 +0100

    kalkulator

--- python/kalkulator.py
+++ python/kalkulator.py
@@ -4,37 +4,147 @@
 #  kalkulator.py
 
 wynik = 0
-liczba1 = 0
-liczba2 = 0
-znak = 0
+znak = ' '   
 
-def pobierzOperacje():
+ 
+def pobierzZnak():
+    global znak 
     znak = (input('Podaj jakie działanie chcesz wykonać: +, - , *, / : '))
-
+    return 0
+    
 def pobierzLiczbe():
-    liczba1 = int(input('Podaj 1 liczbę:' ))
-    liczba2 = int(input('Podaj 2 liczbę'))
+    liczba1 = int(input('Podaj 1 liczbę: '))
+    return liczba1
     
-def dodaj():
-
-def odejmij():
-
-def iloczyn():
-
-def iloraz():
+def pobierzLiczbe2():    
+    liczba2 = int(input('Podaj 2 liczbę: '))
+    return liczba2
     
-def drukujWynik():
-    print(wynik)
-    
-    
-def main(args):
+def dodaj(liczba1, liczba2):
     global wynik
-    pobierzOperacje()
-    pobierzLiczbe()
-    wykonaj()
-    wynik = drukujWynik()
+    wynik = liczba1 + liczba2
+
+def odejmij(liczba1, liczba2):
+    global wynik
+    wynik = liczba1 - liczba2
+
+def iloczyn(liczba1, liczba2):
+    global wynik
+    wynik = liczba1 * liczba2
+
+
+def iloraz(liczba1, liczba2):
+    return liczba1 / liczba2
+
+def drukujWynik():
+    print('Wynik: ', wynik)
+        
+
+def main(args):
+    znak = pobierzZnak()
+    liczba1 = pobierzLiczbe()
+    liczba2 = pobierzLiczbe2()
+    if znak == '+':
+        dodaj(liczba1, liczba2)
+        print(wynik)
+    elif znak == '-':
+        odejmij(liczba1, liczba2)
+        print(wynik)
+    elif znak == '*':
+        iloczyn(liczba1, liczba2)
+        print(wynik)
+    elif znak == '/':
+        iloraz(liczba1, liczba2)
+        print(wynik)
+    else:
+        print('Błędny znak!')
+        
     return 0
 
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
