9e05b890963fa596a60bf33c32500dac579696d9
mkowalikx1337 <mateusz.kowalik@lo1.sandomierz.pl>
2020-02-19 11:41:54 +0100

    kommit

--- CPP/cw2tab.cpp
+++ CPP/cw2tab.cpp
@@ -8,23 +8,51 @@
 
 #include <iostream>
 #include <time.h>
+#include <iomanip>
 using namespace std;
 
 void wypelnij(int t[][10], int w, int k, int n)
 {
     srand(time(NULL));
-    cout << rand() % (n + 1) << endl;
+    for (int i = 0;i < w; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            t[i][j] = rand() % (n + 1); 
+            cout << i << "," << j << " " << t[i][j] << endl;
+        }
+    }
+}
+
+void sumujW(int t[][10], int w, int k, int n)
+{
+    int sumaW = 0;
+    for (int i = 0;i < w; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            cout << setw(4) << t[i][j];
+            sumaW += t[i][j];
+        }
+        cout << setw(6) <<  sumaW << endl;
+    }
+    // wydrukuj maksymalną sumę oraz indeks wiersza
+}
+
+void sumujK(int t[][10], int w, int k, int n)
+{    
 }
 
 int main(int argc, char **argv)
 {
 	int n = 0;
-    //cout << "Podaj maksymalną wartość kumpel: " << endl;
-    //cin >> n;
+    cout << "Podaj maksymalną wartość kumpel: ";
+    cin >> n;
     int w = 5;
     int k = 10;
     int t[w][10];
-    wypelnij(t, w, k, n);
+    //wypelnij(t, w, k, n);
+    sumujW(t, w, k, n);
 	return 0;
 }
 
